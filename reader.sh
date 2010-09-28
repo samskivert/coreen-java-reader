abbcf423145f7dbdb7179ed4eb252979b67bd476
Michael Bayne <mdb@samskivert.com>
2010-09-27 17:02:26 -0700

    Use the non-Proguarded code.

--- reader.sh
+++ reader.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 RDIR=`dirname $0`
-CLASSPATH=`echo $RDIR/target/scala_2.8.0/coreen-java-reader_2.8.0-0.1.min.jar \
+CLASSPATH=`echo $RDIR/project/boot/scala-2.8.0/lib/scala-library.jar \
+    $RDIR/lib_managed/scala_2.8.0/compile/* \
+    $RDIR/target/scala_2.8.0/coreen-java-reader_2.8.0-0.1.jar \
     /usr/local/java/lib/tools.jar | sed 's/ /:/g'`
 exec java -classpath $CLASSPATH coreen.java.Main "$@"
