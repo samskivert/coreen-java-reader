320ed457e136811c31322754b9de7dd0afc3f18b
Michael Bayne <mdb@samskivert.com>
2010-10-02 12:40:28 -0700

    Allow filters to be applied when scanning a project for source files. Hack in a filter when I'm scanning my local JDK to keep things sane.

--- src/main/scala/coreen/java/Main.scala
+++ src/main/scala/coreen/java/Main.scala
@@ -11,27 +11,38 @@ import java.io.File
 object Main
 {
   def main (args :Array[String]) {
-    if (args.length != 1) {
-      die("Usage: coreen.java.Main project_root_dir")
+    if (args.length < 1) {
+      die("Usage: coreen.java.Main project_root_dir [include_prefix ...]")
     }
+
+    // set up a filter function if any path prefixes were provided
+    val filters = if (args(0) == "/usr/local/java") Array("src/java") // hack!
+                  else args drop(1)
+    val filter = if (filters.isEmpty) (path :String) => true
+                 else (path :String) => path.startsWith(args(0)) && {
+                   val relpath = stripFS(path.substring(args(0).length))
+                   filters exists(pre => relpath.startsWith(pre) || pre.startsWith(relpath))
+                 }
 
     // for now we just process every single java file we can find and pass every single jar file
     // we can find on the classpath to the compiler when doing so; in the future we'll be smart
     // and try to read Eclipse and other project files, and probably support a metadata format
     // of our own
     val root = new File(args(0))
-    val files = collectFiles(root)
+    val files = collectFiles(root, filter)
     files get("java") match {
       case Some(javas) => Reader.process(javas, files getOrElse("jar", List())) foreach(println)
       case None => println("Found no .java files in " + root)
     }
   }
 
-  def collectFiles (file :File) :Map[String,List[File]] = {
+  def collectFiles (file :File, filter :(String => Boolean)) :Map[String,List[File]] = {
     def suffix (name :String) = name.substring(name.lastIndexOf(".")+1)
     def collect (file :File) :List[(String,File)] = {
-      if (file.isDirectory) file.listFiles.toList flatMap(collect)
-      else suffix(file.getName) match {
+      if (file.isDirectory) {
+        if (filter(file.getPath)) file.listFiles.toList flatMap(collect)
+        else List()
+      } else suffix(file.getName) match {
         case "java" => List(("java", file.getCanonicalFile))
         case "jar" => List(("jar", file.getCanonicalFile))
         case _ => List()
@@ -39,6 +50,8 @@ object Main
     }
     collect(file) groupBy(_._1) mapValues(_.map(_._2) distinct)
   }
+
+  def stripFS (path :String) = if (path.startsWith(File.separator)) path.substring(1) else path
 
   private def die (msg :String) = {
     System.err.println(msg)
