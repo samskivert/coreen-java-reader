5b505d13d348df73647bf97156a036c6d27567b8
Michael Bayne <mdb@samskivert.com>
2011-03-11 15:36:00 -0800

    Use Proguard plugin that works properly on Mac OS X. Also wrangle our Proguard config such that the javac classes are properly brought in even though they're in classes.jar which is configured (automatically) as a library jar.

--- project/build/CoreenJavaReader.scala
+++ project/build/CoreenJavaReader.scala
@@ -4,13 +4,28 @@ class CoreenJavaReader (info :ProjectInfo) extends DefaultProject(info) with Pro
   val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
   val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.0"
 
-  // we need tools.jar in our classpath because we're building against javac bits
-  val toolsJarPath = Path.fromFile(
-    Path.fileProperty("java.home").asFile.getParent) / "lib" / "tools.jar"
+  // we need to locate tools.jar (Windows/Linux) or classes.jar (Mac OS X)
+  val javaHomeParent = Path.fromFile(Path.fileProperty("java.home").asFile.getParent)
+  val toolsJarPath = javaHomeParent / "lib" / "tools.jar"
+  val classesJarPath = javaHomeParent / "Classes" / "classes.jar"
+  val javacJarPath = if (toolsJarPath.asFile.exists) toolsJarPath
+                     else if (classesJarPath.asFile.exists) classesJarPath
+                     else throw new Exception("Unable to find tools.jar or classes.jar")
+
+  // if we're on Windows/Linux, we need to add tools.jar to our classpath, but on the Mac OS
+  // classes.jar is already part of the classpath so we need not add it
   override def unmanagedClasspath = super.unmanagedClasspath +++ toolsJarPath
 
+  // on the Mac we end up using classes.jar as both an input jar *and* a library jar, but we
+  // mitigate the ensuing hilarity by filtering all but the javac code from classes.jar when using
+  // it as an input jar; this causes Proguard to behave mostly sanely
+  override def makeInJarFilter (file :String) = file match {
+    case "classes.jar" => "com/sun/tools/**,com/sun/source/**"
+    case _ => super.makeInJarFilter(file)
+  }
+
   // proguard plugin configurations
-  override def proguardInJars = super.proguardInJars +++ toolsJarPath
+  override def proguardInJars = super.proguardInJars +++ javacJarPath
   override def proguardLibraryJars = super.proguardLibraryJars +++ scalaLibraryPath
   override def proguardOptions = List(
     "-dontnote scala.**,sun.tools.**,sun.applet.**",
